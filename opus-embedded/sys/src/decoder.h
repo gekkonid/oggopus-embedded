119fd223bfef83c57bcff032bba08b46e1f53103
Teodor Wozniak <teo-git@lumifaza.org>
2026-02-25 17:43:40 +0100

    Revert alignment as it yields mixer results

--- opus-embedded/sys/src/decoder.h
+++ opus-embedded/sys/src/decoder.h
@@ -6,10 +6,9 @@
  * <div rustbindgen replaces="OpusDecoder"></div>
  */
 struct OpusDecoder {
-    // aligned(16) yields better performance than aligned(4), at least on xtensa
 #ifdef OPUS_EMBEDDED_SYS_STEREO
-    char _unused[26580] __attribute__((aligned(16)));
+    char _unused[26580] __attribute__((aligned(4)));
 #else
-    char _unused[17860] __attribute__((aligned(16)));
+    char _unused[17860] __attribute__((aligned(4)));
 #endif
 };
